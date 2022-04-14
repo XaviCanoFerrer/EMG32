a2fd1c47edc727293cd9bc5d8b72f2fe2ca3b1be
XaviGithubCF <xavier.cano-ferrer@crick.ac.uk>
2022-04-14 20:22:39 +0100

    Update EMG_ESP32.ino

--- Firmware/EMG_ESP32/EMG_ESP32.ino
+++ Firmware/EMG_ESP32/EMG_ESP32.ino
@@ -1,3 +1,18 @@
+/*
+  ----------------------------------------------------------------------------
+  This file is part of the EMG32 repository
+  Written by Xavier Cano-Ferrer
+  ----------------------------------------------------------------------------
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, version 3.
+  This program is distributed  WITHOUT ANY WARRANTY and without even the
+  implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  See the GNU General Public License for more details.
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <Adafruit_ADS1X15.h>
 
 Adafruit_ADS1015 ads;     /* Use this for the 12-bit ADC version */
